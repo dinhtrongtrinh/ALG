7c520e76bc78ec76019a97d9a0bc543a465d2fec
Trong Trinh Dinh <dinhtron@student.cvut.cz>
2025-09-23 11:04:26 +0200

    Trying brute force the park

--- Projektování přírodního parku/main.cpp
+++ Projektování přírodního parku/main.cpp
@@ -2,31 +2,50 @@
 #include <vector>
 
 using namespace std;
-
+int max_tree = 0; 
+/*
+bool checking_for_S(vector<vector<int>>& park, int xstart, int ystart, int xend, int yend, int S){
+    int sum = 0;
+    for (int i = xstart; i <=  xend; i++){
+        for (int j = ystart; j <=  yend; j++){
+            if (park[i][j] == 2){
+                sum++;
+            }
+        }
+    }
+    if (sum >= S){
+        return true;
+    }
+    return false;
+}
+*/
+int checking_tree( vector<vector<int>>& park, int N, int K, int L, int S){
+    // checking for valid park areas
+    int current_S = 0; 
+    int valid_park_size_end = N - L;
+    for(int i = L ; i < valid_park_size_end; i++){
+        for(int j = L; j < valid_park_size_end; j++){
+            
+        }
+    }    
+    return max_tree;
+}
 
 
 
 int main (){
     int N, K, L, S;
-
     cin >> N >> K >> L >> S;
-    vector<vector<int>> park(N, vector<int>(N, 0));
+
+    vector<vector<int>> park(N, vector<int>(N)); // N x N matrix
+
     for (int i = 0; i < N; i++) {
-        vector<int> row(N);
         for (int j = 0; j < N; j++) {
-            int x;
-            cin >> x;
-            row.push_back(x);
+            cin >> park[i][j]; // directly fill the matrix
         }
-        park.push_back(row);
     }
-    //for debug print
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            cout << park[i][j] << " ";
-        }
-        cout << endl;
-    }
+    int result = checking_tree(park, N, K, L, S);
+    cout << result << endl;
     
 
     return 0;
